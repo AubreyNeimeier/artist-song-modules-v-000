32375e210e1823cdeaa18b6abb288967ce21f631
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-29 17:37:25 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,12 +2,13 @@ require 'pry'
 
 class Artist
   extend Memorable
+  extend Findable
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
-
+  
 
   def initialize
     @@artists << self
