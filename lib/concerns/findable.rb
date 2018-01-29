55d2a883e3551309b4375203d801365525b87217
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-29 17:36:46 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,7 +3,9 @@ module Findable
     self.all.detect{|a| a.name == name}
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+=begin
+  def find_by_name(name)
+    self.all.detect{|a| a.name == name}
   end
+=end
 end
