6326be9e011cfbce1dcf9fa95b8f05e2d80215f6
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-29 17:56:00 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,8 +13,9 @@ class Artist
 
 
   def initialize
+    #initialize is an instance method.
     #@@artists << self
-    self.class.all << self 
+    self.class.all << self
     #similar to calling Artist.all or Song.all. we call up the class of the instance with .class
     @songs = []
   end
